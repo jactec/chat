0ded4bed07337dc088428f05a5965eeaae520b08
jactec <someone@somewhere.nowhere>
2021-12-29 12:00:35 +1100

    print Fibonacci

--- Print.java
+++ Print.java
@@ -3,12 +3,31 @@
 class Print {
 
     public static void main(String[] args) {
-        print1_100();
+        printFibonacci();
     }
 
+    private static void printFibonacci() {
+        int n = 10;
+        long[] fib = generateFibonaccis(n);
+        for (int i = 0; i < n; i++) {
+            System.out.print(fib[i] + ",");
+        }
+    }
+
+    public static long[] generateFibonaccis(int n) {
+        long[] fib = new long[n];
+        fib[0] = 1;
+        fib[1] = 1;
+        for (int i = 2; i < n; ++i) {
+            fib[i] = fib[i - 2] + fib[i - 1];
+        }
+        return fib;
+    }
+    
     private static void print1_100() {
         for(int i=1;i<=100;i=i+2) {  // revised print even numbers from 0-100
             System.out.println(i); 
         }
     }
+
 }
