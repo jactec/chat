ce668a17f19556a773afed768edb54dcd81b048c
dlij <82123948+dlij@users.noreply.github.com>
2021-12-29 11:32:16 +1100

    Q1 Answered

--- Print.java
+++ Print.java
@@ -3,9 +3,12 @@
 class Print {
 
     public static void main(String[] args) {
+        print1_100();
+    }
 
-        String variable = "Hello, World!";
-
-        System.out.println(variable); 
+    private static void print1_100() {
+        for(int i=1;i<=100;i=i+1) {
+            System.out.println(i); 
+        }
     }
 }
