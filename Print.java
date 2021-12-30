b307515b101bea1e5967ca43cf88fe15d6ff2085
dlij <82123948+dlij@users.noreply.github.com>
2021-12-30 15:27:11 +1100

    fibonacci first 100

--- Print.java
+++ Print.java
@@ -3,12 +3,24 @@
 class Print {
 
     public static void main(String[] args) {
-        System.out.println(1);
-        System.out.println(1); 
-        System.out.println(2);
-        System.out.println(3);
-        System.out.println(5);
-        System.out.println(8);
-        // print first 6 numbers of fibonacci. Looking at how to print first 100 numbers of fibonacci;
+        printFibonacci();
+    }
+
+    private static void printFibonacci() {
+        int n = 100;
+        long[] fib = generateFibonaccis(n);
+        for (int i = 0; i < n; i++) {
+            System.out.print(fib[i] + ",");
+        }
+    }
+
+    public static long[] generateFibonaccis(int n) {
+        long[] fib = new long[n];
+        fib[0] = 1;
+        fib[1] = 1;
+        for (int i = 2; i < n; ++i) {
+            fib[i] = fib[i - 2] + fib[i - 1];
+        }
+        return fib;
     }
 }
